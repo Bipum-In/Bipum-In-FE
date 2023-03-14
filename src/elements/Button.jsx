1215e08d7e03a6b9726ed7d59ebe4a550c012073
Seogun95 <ksj083895@gmail.com>
2023-03-14 22:50:52 +0900

    design: button 세미콜론 추가 및 logo.svg 추가

--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -37,25 +37,14 @@ const ButtonWrapper = styled.button`
   white-space: nowrap;
 
   ${props =>
-    props.reg &&
-    css`
-      padding: 0.3rem 0.5rem;
-      font-size: 0.75rem;
-      color: white;
-      background: blue;
-      height: 100%;
-    `}
-
-  ${props =>
     props.menuStyle
       ? css`
           width: 15rem;
           height: 3.125rem;
           color: white;
-          background-color: ${props => props.theme.color.blue.brandColor6}
+          background-color: ${props => props.theme.color.blue.brandColor6};
           margin: 0 0.6875rem;
-          border: 0.0579rem solid ${props =>
-            props.theme.color.grey.brandColor2};
+          border: 0.0579rem solid ${props => props.theme.color.grey.brandColor2};
           box-shadow: 0.2314rem 0.2314rem 1.1571rem rgba(0, 0, 0, 0.1);
           border-radius: 0.4631rem;
           font-weight: 600;
@@ -73,7 +62,6 @@ const ButtonWrapper = styled.button`
           font-weight: 400;
           font-size: 1.25rem;
         `}
-
 
   & {
     cursor: pointer;
