b7576e811377f4e6ee6a9d78559b757598a6f601
Seogun95 <ksj083895@gmail.com>
2023-03-14 16:58:15 +0900

    Revert "design: sidbar 틀 작업"
    This reverts commit 15eb90f80255d937da9bed552f1ddfddeea01117.

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-// import Header from './Header';
+import Header from './Header';
 import Sidebar from './Sidebar';
 
 export default function AdminLayout() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Sidebar />
       <Outlet />
     </>
