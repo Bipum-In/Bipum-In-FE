0f9e4451088f5b22db5ecefc703c4197e62d150f
Adult96 <djxor007@gmail.com>
2023-03-15 01:32:14 +0900

    fix: inputLayout 컴포넌트 삭제

--- src/elements/Input.jsx
+++ src/elements/Input.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const Input = React.forwardRef((props, ref) => (
-  <InputLayout>
-    <InputStyle {...props} ref={ref} />
-  </InputLayout>
+  <InputStyle {...props} ref={ref} />
 ));
 
 Input.defaultProps = {
@@ -19,56 +17,21 @@ Input.defaultProps = {
 };
 
 const InputStyle = styled.input`
-  ${(props) => props.theme.FlexRow};
-  ${(props) => props.theme.FlexCenter};
+  ${props => props.theme.FlexRow};
+  ${props => props.theme.FlexCenter};
 
-  width: ${(props) => props.w};
-  height: ${(props) => props.h};
-  font-size: ${(props) => props.size};
+  width: ${props => props.w};
+  height: ${props => props.h};
+  font-size: ${props => props.size};
 
   //기본 값
-  padding: ${(props) => props.padding};
-  margin: ${(props) => props.margin};
-  border-radius: ${(props) => props.borderR};
-  border: ${(props) => props.border};
-  background-color: ${(props) => props.bg};
-  transition: ${(props) => props.ts};
-  color: ${(props) => props.color};
-
-  ${(props) =>
-    props.reg &&
-    css`
-      flex: 1 1 auto;
-      width: 100%;
-    `}
+  padding: ${props => props.padding};
+  margin: ${props => props.margin};
+  border-radius: ${props => props.borderR};
+  border: ${props => props.border};
+  background-color: ${props => props.bg};
+  transition: ${props => props.ts};
+  color: ${props => props.color};
 `;
 
 export default Input;
-
-export function InputLayout(prop) {
-  return (
-    <InputWrapper>
-      <InputContainer {...prop}>{prop.children}</InputContainer>
-    </InputWrapper>
-  );
-}
-
-InputLayout.defaultProps = {
-  border: 'gray',
-  borderR: '.5rem',
-};
-
-const InputWrapper = styled.div`
-  ${(props) => props.theme.FlexCol}
-  width: 100%;
-`;
-const InputContainer = styled.div`
-  position: relative;
-  ${(props) => props.theme.FlexRow}
-  ${(props) => props.theme.FlexCenter}
-  flex: 0 0 30px;
-  background-color: ${(props) => props.theme.color.white};
-  border-radius: ${(props) => props.borderR};
-  border: 1px solid ${(props) => props.border};
-  transition: 0.1s;
-`;
