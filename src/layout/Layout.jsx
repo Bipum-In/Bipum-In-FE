754814e63a87028de54507180b0a16a327d9dfa4
Seogun95 <ksj083895@gmail.com>
2023-03-15 04:15:05 +0900

    design: button 스타일, layout margin 변경

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { styled } from 'styled-components';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
-export default function AdminLayout() {
+export default function DashBoardLayout() {
   return (
-    <>
+    <LayoutWrapper>
       <Header />
       <Sidebar />
-      <Outlet />
-    </>
+      <OutletContainer>
+        <Outlet />
+      </OutletContainer>
+    </LayoutWrapper>
   );
 }
+
+const LayoutWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+const OutletContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 120px;
+`;
