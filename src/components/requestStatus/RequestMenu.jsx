17e013c1bfb480af5fceafc268ae4b2ee582719c
Adult96 <djxor007@gmail.com>
2023-03-14 19:19:07 +0900

    design: RequestMenu 스타일 마크업

--- src/components/requestStatus/RequestMenu.jsx
+++ src/components/requestStatus/RequestMenu.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '../../elements/Button';
 
 export default function RequestMenu() {
+  const [menuStyle, setMenuClick] = useState({
+    all: true,
+    equipment: false,
+    return: false,
+    repair: false,
+  });
+
   return (
     <RequestMenuContainer>
-      <Button reg>전체</Button>
-      <Button>비품 요청</Button>
-      <Button>반납 요청</Button>
-      <Button>수리 요청</Button>
+      <Button menuStyle={menuStyle.all}>전체</Button>
+      <Button menuStyle={menuStyle.equipment}>비품 요청</Button>
+      <Button menuStyle={menuStyle.return}>반납 요청</Button>
+      <Button menuStyle={menuStyle.repair}>수리 요청</Button>
     </RequestMenuContainer>
   );
 }
 
 const RequestMenuContainer = styled.div`
   display: flex;
-  width: 100%;
+  align-items: center;
+  margin: 2.25rem 0 1.5rem 3.25rem;
 `;
