208df68b4680b559301e5a2cfc648077bbf50c58
Adult96 <djxor007@gmail.com>
2023-03-15 20:12:56 +0900

    feat: 리덕스 모듈 폴더 안에 requestStatus 파일 리덕스 모듈 양식에 맞춰서 수정 및 파일 이름과 configStore 수정

---
+++ src/redux/modules/requestStatus.js
@@ -0,0 +1,34 @@
+import Axios from '../../api/axios';
+import Redux from '../redux';
+
+const initialState = {
+  requestStatus: {
+    getRequest: [],
+    isStatusLoading: false,
+    isStatusError: false,
+  },
+};
+
+const axios = new Axios(process.env.REACT_APP_SERVER_URL);
+
+export const __requestStatus = Redux.asyncThunk('REQUEST', payload =>
+  axios.get(`/api/admin/requests?status=${payload.status}&page=${payload.page}`)
+);
+
+const requestStatusSlice = Redux.slice(
+  'getRequest',
+  initialState,
+  {},
+  bulider => {
+    Redux.extraReducer(
+      bulider,
+      __requestStatus,
+      'requestStatus',
+      'getRequest',
+      'isStatusLoading',
+      'isStatusError'
+    );
+  }
+);
+
+export default requestStatusSlice.reducer;
