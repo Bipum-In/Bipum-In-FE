6e4509b1e92ad33491c19aadbb4be46ea74b74dd
Adult96 <djxor007@gmail.com>
2023-03-15 14:11:26 +0900

    feat: menu click시 boolean값 변경 훅함수 기능 구현

---
+++ src/hooks/useSelectMenu.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export default function useSelectMenu(list) {
+  const [menuStyle, setMenuStyle] = useState(list);
+
+  const handleClickMenu = e => {
+    const menuName = e.target.value;
+    setMenuStyle(selectMenu(menuName));
+  };
+
+  const selectMenu = menuName => {
+    return menuStyle.map(list =>
+      list.name === menuName
+        ? { ...list, status: true }
+        : { ...list, status: false }
+    );
+  };
+
+  return [menuStyle, handleClickMenu];
+}
