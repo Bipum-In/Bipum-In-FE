047eb27eab1e4786efd49340eb67fc64c853160f
Seogun95 <ksj083895@gmail.com>
2023-03-15 18:36:38 +0900

    feat: header 구성 완료 및 seletbox 추가

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useRef } from 'react';
+import styled, { css } from 'styled-components';
 import { ReactComponent as Search } from '../styles/commonIcon/search.svg';
+import { ReactComponent as Alaram } from '../styles/commonIcon/alarm.svg';
+import { ReactComponent as Settings } from '../styles/commonIcon/settings.svg';
+import { ReactComponent as ArrowDown } from '../styles/commonIcon/arrowDown.svg';
+import useOutsideClick from '../hooks/useOutsideClick';
+import test from '../styles/test.png';
+export default function Header() {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
-export default function header() {
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
+  const dropDownRef = useRef(null);
+  useOutsideClick(dropDownRef, () => {
+    setIsDropdownVisible(false);
+  });
+
   return (
     <HeaderWrapper>
       <HeaderContainer>
         <ItemContainer>
+          {/* 검색창 */}
           <SearchContainer>
-            <SearchIconContainer>
+            <IconContainer search="true">
               <Search />
-            </SearchIconContainer>
+            </IconContainer>
             <SearchInput placeholder="검색어를 입력하세요." />
           </SearchContainer>
+          {/* 헤더 오른쪽 아이템 */}
+          <HeaderRightContainer>
+            <IconContainer>
+              <Alaram />
+            </IconContainer>
+            <IconContainer>
+              <Settings />
+            </IconContainer>
+            {/* 드롭다운 컨테이너 */}
+            <LoginUserInfoDropDown
+              onClick={toggleDropdown}
+              className={isDropdownVisible ? 'visible' : ''}
+              ref={dropDownRef}
+            >
+              <UserImgContainer />
+              <UserInfoDetailContainer>
+                <InfoCompanyTitle>AP코퍼레이션</InfoCompanyTitle>
+                <InfoUserName>김선중 관리자님</InfoUserName>
+              </UserInfoDetailContainer>
+              <UserDropDown isRotated={isDropdownVisible}>
+                <ArrowDown />
+              </UserDropDown>
+              {/* 드롭다운 디테일 */}
+              <DropdownContainer></DropdownContainer>
+            </LoginUserInfoDropDown>
+          </HeaderRightContainer>
         </ItemContainer>
       </HeaderContainer>
     </HeaderWrapper>
@@ -29,15 +71,20 @@ const HeaderWrapper = styled.header`
 `;
 
 const HeaderContainer = styled.div`
-  display: flex;
-  align-items: center;
   width: calc(100vw - 250px);
   margin-left: auto;
   height: 100%;
 `;
 
 const ItemContainer = styled.div`
+  ${props => props.theme.FlexRow};
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
   margin: 0 3.25rem;
+  * svg {
+    cursor: pointer;
+  }
 `;
 
 const SearchContainer = styled.div`
@@ -49,12 +96,20 @@ const SearchContainer = styled.div`
   border-radius: 0.5rem;
 `;
 
-const SearchIconContainer = styled.div`
+const IconContainer = styled.div`
   ${props => props.theme.FlexRow};
   ${props => props.theme.FlexCenter};
   width: 1.875rem;
   height: 1.875rem;
-  margin: 0 0.8125rem 0 1.3125rem;
+  ${props =>
+    props.search === 'true' &&
+    css`
+      margin: 0 0.8125rem 0 1.3125rem;
+    `}
+  svg {
+    width: 1.75rem;
+    height: 1.75rem;
+  }
 `;
 
 const SearchInput = styled.input`
@@ -63,4 +118,82 @@ const SearchInput = styled.input`
   line-height: 1.3125rem;
   font-size: 1.125rem;
   color: ${props => props.theme.color.grey.brandColor7};
+`;
+
+const HeaderRightContainer = styled.div`
+  ${props => props.theme.FlexRow};
+  ${props => props.theme.FlexCenter};
+  margin-left: auto;
+  gap: 1.875rem;
+`;
+
+const LoginUserInfoDropDown = styled.div`
+  position: relative;
+  ${props => props.theme.FlexRow};
+  align-items: center;
+  padding: 0 0.375rem;
+  width: 100%;
+  height: 3.75rem;
+  gap: 0.375rem;
+  background-color: white;
+  border: 0.0625rem solid ${props => props.theme.color.grey.brandColor2};
+  border-radius: 0.5rem;
+  cursor: pointer;
+`;
+
+const UserImgContainer = styled.div`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 0.375rem;
+  background: url(${test}) no-repeat center center / cover,
+    ${props => props.theme.color.grey.brandColor2};
+`;
+
+const UserInfoDetailContainer = styled.div`
+  ${props => props.theme.FlexCol};
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.25rem;
+`;
+const InfoCompanyTitle = styled.span`
+  font-size: 0.8125rem;
+  color: ${props => props.theme.color.grey.brandColor6};
+`;
+
+const InfoUserName = styled.span`
+  font-weight: 600;
+  font-size: 1.125rem;
+  line-height: 1.3125rem;
+`;
+
+const UserDropDown = styled.div`
+  ${props => props.theme.FlexRow};
+  ${props => props.theme.FlexCenter};
+  width: 1.5rem;
+  height: 1.5rem;
+  transform: ${({ isRotated }) =>
+    isRotated ? 'rotate(180deg)' : 'rotate(0deg)'};
+  transition: transform 0.3s ease;
+  backface-visibility: hidden;
+  will-change: transform;
+`;
+
+const DropdownContainer = styled.div`
+  position: absolute;
+  top: 110%;
+  right: 0;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.15);
+  border-radius: 0.25rem;
+  padding: 1rem;
+  z-index: 10;
+
+  opacity: 0;
+  transform: translateY(-1.25rem);
+  transition: opacity 0.3s ease, transform 0.3s ease;
+  ${LoginUserInfoDropDown}.visible & {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
