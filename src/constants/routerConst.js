7bbef9276097a6efe9b0d4f4c26773aac11d4a5d
Adult96 <djxor007@gmail.com>
2023-03-18 00:23:27 +0900

    feat: EquipmentManagement 컴포넌트 생성및 라우터 설정 및 상수 경로 설정

--- src/constants/routerConst.js
+++ src/constants/routerConst.js
@@ -4,6 +4,7 @@ const PATH = {
   LOGIN: '/login',
   ADMIN_DASHBOARD: '/admin-dashboard',
   ADMIN_REQUEST_STATUS: '/request-status',
+  ADMIN_EQUIPMENT_MANAGEMENT: '/equipment-management',
   ADMIN_EQUIPMENT_ADD: '/equipment-add',
   USER_DASHBOARD: '/user-dashboard',
   RENDING: '/rending',
