f15a2459423f454b26c07cf8428265373d9ee28d
9mean2 <o3omoomin@gmail.com>
2023-03-22 02:41:57 +0900

    feat: partnerList 스토어 연걸, 리덕스 생성

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import requestStatus from '../modules/requestStatus';
 import dashboardStatus from '../modules/dashboardStatus';
 import equipmentStatus from '../modules/equipmentStatus';
+import partnersList from '../modules/partnersList';
 
 const store = configureStore({
   reducer: {
+    partnersList,
     requestStatus,
     dashboardStatus,
     equipmentStatus,
