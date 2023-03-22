72851c0f6f80956d323e156148cce5ee0c8d987c
9mean2 <o3omoomin@gmail.com>
2023-03-23 01:53:30 +0900

    feat: 부서목록get 슬라이스 추가 , 리덕스 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -3,9 +3,11 @@ import requestStatus from '../modules/requestStatus';
 import dashboardStatus from '../modules/dashboardStatus';
 import equipmentStatus from '../modules/equipmentStatus';
 import partnersList from '../modules/partnersList';
+import deptList from '../modules/deptList';
 
 const store = configureStore({
   reducer: {
+    deptList,
     partnersList,
     requestStatus,
     dashboardStatus,
