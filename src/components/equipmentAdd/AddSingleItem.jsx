7b1c9ee503fae59b286ceb800bb46ae462b1600e
9mean2 <o3omoomin@gmail.com>
2023-03-23 21:58:41 +0900

    feat: 부서별 유저 조회

--- src/components/equipmentAdd/AddSingleItem.jsx
+++ src/components/equipmentAdd/AddSingleItem.jsx
@@ -7,32 +7,36 @@ import { v4 as uuidv4 } from 'uuid';
 import { getCategoryList } from '../../redux/modules/equipmentStatus';
 import { __partnersList } from '../../redux/modules/partnersList';
 import SelectBoxs from '../common/SelectBoxs';
+import { __deptList } from '../../redux/modules/deptList';
+import { __deptUserList } from '../../redux/modules/deptUserList';
+import Axios from '../../api/axios';
+
+const axios = new Axios(process.env.REACT_APP_SERVER_URL);
 
 export default function AddSingleItem() {
   const dispatch = useDispatch();
+  const [dept, setDept] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     dispatch(getCategoryList());
     dispatch(__partnersList());
   }, [dispatch]);
 
-  const { getPartners, isPartnersLoading, isPartnersError } = useSelector(
+  useEffect(() => {
+    axios.get(`/api/dept`).then(response => setDept(response.data.data));
+  }, []);
+
+  const { getPartners, isPartnersLoading } = useSelector(
     state => state.partnersList.partnersList
   );
-
   const partnersData = getPartners?.data;
-
-  const { getCategory, isCategoryLoading, isCategoryError } = useSelector(
+  const { getCategory, isCategoryLoading } = useSelector(
     state => state.equipmentStatus.category
-  );
-
-  const largeCategories = Array.from(
-    new Set(getCategory?.category.map(item => item.largeCategory))
   );
 
   const [formImage, setFormformImage] = useState(null);
   const [preview, setPreview] = useState('');
-  // const dispatch = useDispatch();
   function onChangeimge(e) {
     const img = e.target.files[0];
     const formImg = new FormData();
@@ -49,6 +53,22 @@ export default function AddSingleItem() {
       setPreview('');
     }
   }
+
+  const onValid = data => {
+    const formData = new FormData();
+    // formData.append('largeCategory');
+    // formData.append('categoryName');
+    // formData.append('modelName');
+    // formData.append('serialNum');
+    // formData.append('createdAt');
+    // formData.append('partnersId');
+    // formData.append('userId');
+    for (const keyValue of formImage) {
+      formData.append(keyValue[0], keyValue[1]);
+    }
+    console.log(data);
+  };
+
   const getDaysInMonth = (year, month) => {
     const date = new Date(year, month, 1);
     const lastDay = new Date(
@@ -58,35 +78,38 @@ export default function AddSingleItem() {
     ).getDate();
     return Array.from({ length: lastDay }, (_, i) => i + 1);
   };
+
   const [nameValue, setNameValue] = useState('');
   const [serialValue, setSerialValue] = useState('');
-
   const [selectedLargeCategory, setSelectedLargeCategory] = useState('대분류');
-  // const [mainTypeSeleteValue, setMainTypeSeleteValue] = useState('대분류');
   const [isShowOptions, setShowOptions] = useState(false);
   const handleOnChangeMainTypeSelectValue = e => {
     const { innerText } = e.target;
     setSelectedLargeCategory(innerText);
-    // setSelectedLargeCategory(innerText);
   };
 
   const [subTypeSeleteValue, setSubTypeSeleteValue] = useState('소분류');
+
   const [isShowSubOptions, setShowSubOptions] = useState(false);
   const handleOnChangeSubTypeSelectValue = e => {
     const { innerText } = e.target;
     setSubTypeSeleteValue(innerText);
   };
 
-  const categoryData = getCategory?.category.filter(
-    item => item.largeCategory === selectedLargeCategory
-  );
+  const categoryData =
+    getCategory?.category &&
+    getCategory.category.filter(
+      item => item.largeCategory === selectedLargeCategory
+    );
 
   const [yearSeleteValue, setYearSeleteValue] = useState('');
+
   const [isShowYearOptions, setShowYearOptions] = useState(false);
   const handleYearChange = e => {
     const { innerText } = e.target;
     setYearSeleteValue(innerText);
   };
+
   const yearOptions = [];
   for (let year = 2000; year <= 2023; year++) {
     yearOptions.push(
@@ -95,6 +118,7 @@ export default function AddSingleItem() {
       </Option>
     );
   }
+
   const [monthSeleteValue, setMonthSeleteValue] = useState('');
   const [isShowMonthOptions, setShowMonthOptions] = useState(false);
   const handleMonthChange = e => {
@@ -110,27 +134,44 @@ export default function AddSingleItem() {
     );
   }
   const [daySeleteValue, setDaySeleteValue] = useState('');
+
   const [isShowDayOptions, setShowDayOptions] = useState(false);
   const handleDayChange = e => {
     const { innerText } = e.target;
     setDaySeleteValue(innerText);
   };
   const [partnerSeleteValue, setPartnerSeleteValue] = useState('회사명');
+
+  const [partnerId, setPartnerId] = useState(null);
   const [isShowPartnerOptions, setShowPartnerOptions] = useState(false);
+
   const handleOnChangePartnerSelectValue = e => {
-    const { innerText } = e.target;
+    const { innerText, value } = e.target;
+    setPartnerId(value);
     setPartnerSeleteValue(innerText);
   };
+
   const [depSeleteValue, setDepSeleteValue] = useState('부서명');
+
   const [isShowDepOptions, setShowDepOptions] = useState(false);
+
   const handleOnChangeDepSelectValue = e => {
     const { innerText } = e.target;
     setDepSeleteValue(innerText);
+
+    const deptId = e.target.value;
+    axios
+      .get(`/api/user/${deptId}`)
+      .then(response => setUser(response.data.data));
   };
+  const [userId, setUserId] = useState(null);
   const [nameSeleteValue, setNameSeleteValue] = useState('이름');
+  console.log(userId);
+
   const [isShowNameOptions, setShowNameOptions] = useState(false);
   const handleOnChangeNameSelectValue = e => {
-    const { innerText } = e.target;
+    const { innerText, value } = e.target;
+    setUserId(value);
     setNameSeleteValue(innerText);
   };
 
@@ -138,7 +179,7 @@ export default function AddSingleItem() {
     <>
       {getCategory && (
         <Container>
-          <AddContainer>
+          <AddContainer onSubmit={onValid}>
             <EquipmentContainer>
               <TypeBox>
                 <TypeTitle requiredinput="true">비품종류</TypeTitle>
@@ -152,16 +193,17 @@ export default function AddSingleItem() {
                   height="true"
                   arrow="true"
                 >
-                  {largeCategories.map(item => {
-                    return (
-                      <Option
-                        key={uuidv4()}
-                        onClick={handleOnChangeMainTypeSelectValue}
-                      >
-                        {item}
-                      </Option>
-                    );
-                  })}
+                  {getCategory.largeCategory.map(
+                    item =>
+                      item.name !== '전체' && (
+                        <Option
+                          key={uuidv4()}
+                          onClick={handleOnChangeMainTypeSelectValue}
+                        >
+                          {item.name}
+                        </Option>
+                      )
+                  )}
                 </SelectBoxs>
                 <SelectBoxs
                   onClick={() =>
@@ -189,7 +231,7 @@ export default function AddSingleItem() {
                 <Input
                   type="text"
                   value={nameValue}
-                  onChange={event => setNameValue(event.target.value)}
+                  setState={event => setNameValue(event.target.value)}
                   placeholder="제품명을 기입해주세요"
                   required
                 />
@@ -199,7 +241,7 @@ export default function AddSingleItem() {
                 <Input
                   type="text"
                   value={serialValue}
-                  onChange={event => setSerialValue(event.target.value)}
+                  setState={event => setSerialValue(event.target.value)}
                   placeholder="시리얼넘버를 기입해주세요"
                   required
                 />
@@ -250,7 +292,7 @@ export default function AddSingleItem() {
                           new Date().getFullYear(),
                           parseInt(monthSeleteValue) - 1
                         ).map(day => (
-                          <Option key={uuidv4()}>{day}</Option>
+                          <Option key={uuidv4()}> {day}</Option>
                         ))}
                       </Option>
                     </SelectBoxs>
@@ -274,6 +316,7 @@ export default function AddSingleItem() {
                       return (
                         <Option
                           key={uuidv4()}
+                          value={item.partnersId}
                           onClick={handleOnChangePartnerSelectValue}
                         >
                           {item.partnersName}
@@ -295,7 +338,17 @@ export default function AddSingleItem() {
                     show={isShowDepOptions}
                     height="true"
                   >
-                    <Option onClick={handleOnChangeDepSelectValue}>선택</Option>
+                    {dept?.map(item => {
+                      return (
+                        <Option
+                          key={uuidv4()}
+                          onClick={handleOnChangeDepSelectValue} //수정
+                          value={item.deptId}
+                        >
+                          {item.deptName}
+                        </Option>
+                      );
+                    })}
                   </SelectBoxs>
                 </DepName>
                 <UserName>
@@ -309,9 +362,15 @@ export default function AddSingleItem() {
                     show={isShowNameOptions}
                     height="true"
                   >
-                    <Option onClick={handleOnChangeNameSelectValue}>
-                      사우명?
-                    </Option>
+                    {user?.map(user => (
+                      <Option
+                        key={uuidv4()}
+                        value={user.userId}
+                        onClick={handleOnChangeNameSelectValue}
+                      >
+                        {user.empName}
+                      </Option>
+                    ))}
                   </SelectBoxs>
                 </UserName>
               </TypeBox>
@@ -364,7 +423,7 @@ const UserName = styled.div`
   width: 5.8125rem;
   height: 2.5rem;
 `;
-const Option = styled.ul`
+const Option = styled.li`
   font-size: 0.875rem;
   padding: 0.375rem 0.5rem;
   transition: background-color 0.2s ease-in;
@@ -374,7 +433,6 @@ const Option = styled.ul`
 `;
 const ButtonBox = styled.div`
   position: absolute;
-  /* border: 1px solid red; */
   right: 0rem;
   bottom: 3.5rem;
   display: flex;
