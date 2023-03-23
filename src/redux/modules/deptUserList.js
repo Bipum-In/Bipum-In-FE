10debd11f9cf5d6d93fac9ddc9392a8676ae7f62
9mean2 <o3omoomin@gmail.com>
2023-03-23 16:58:52 +0900

    feat: 부서별 사원조회get , 리덕스 추가

---
+++ src/redux/modules/deptUserList.js
@@ -0,0 +1,34 @@
+import Axios from '../../api/axios';
+import Redux from '../redux';
+
+const initialState = {
+  deptUserList: {
+    getDeptUser: null,
+    isDeptUserLoading: false,
+    isDeptUserError: false,
+  },
+};
+
+const axios = new Axios(process.env.REACT_APP_SERVER_URL);
+
+export const __deptUserList = Redux.asyncThunk('DEPTUSER', payload =>
+  axios.get(`/api/user/${payload.deptId}`)
+);
+
+const deptUserListSlice = Redux.slice(
+  'getDeptUser',
+  initialState,
+  {},
+  bulider => {
+    Redux.extraReducer(
+      bulider,
+      __deptUserList,
+      'deptUserList',
+      'isDeptUserLoading',
+      'getDeptUser',
+      'isDeptUserError'
+    );
+  }
+);
+
+export default deptUserListSlice.reducer;
