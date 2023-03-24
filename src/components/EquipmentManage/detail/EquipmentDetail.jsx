eff7f98dba72ef2eabd727573770f9fbcfe262b0
Adult96 <djxor007@gmail.com>
2023-03-25 00:05:51 +0900

    design: EquipmentDetail header 및 body title 스타일 구현

--- src/components/EquipmentManage/detail/EquipmentDetail.jsx
+++ src/components/EquipmentManage/detail/EquipmentDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import Button from '../../../elements/Button';
 import { getEquipmentDetail } from '../../../redux/modules/equipmentStatus';
 
 export default function EquipmentDetail({ detailId }) {
@@ -13,10 +14,82 @@ export default function EquipmentDetail({ detailId }) {
     dispatch(getEquipmentDetail(detailId));
   }, [detailId, dispatch]);
 
-  return <DetailWrapper></DetailWrapper>;
+  return (
+    <>
+      {getDetail && (
+        <DetailWrapper>
+          <DetailHeader>
+            <Button>폐기</Button>
+            <Button>수정</Button>
+            <Button>저장</Button>
+          </DetailHeader>
+          <DetailBodyTitle>
+            <span>{getDetail.supplyDetail.modelName}</span>
+            <span>
+              <Status />
+              {getDetail.supplyDetail.status}
+            </span>
+          </DetailBodyTitle>
+        </DetailWrapper>
+      )}
+    </>
+  );
 }
 
 const DetailWrapper = styled.main`
-  ${props => props.theme.wh100}
+  ${props => props.theme.flexCol}
+
   padding: 0 6.375rem;
+`;
+
+const DetailHeader = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin: 2.25rem 0;
+  gap: 0.5rem;
+
+  button {
+    width: 3.9375rem;
+    height: 2.0625rem;
+    margin: 0;
+  }
+
+  button:nth-child(1) {
+    color: #6d5517;
+    background-color: #efecd9;
+  }
+
+  button:nth-child(2),
+  button:nth-child(3) {
+    color: white;
+    background-color: ${props => props.theme.color.blue.brandColor6};
+  }
+`;
+
+const DetailBodyTitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.4375rem;
+
+  span:first-child {
+    font-weight: 600;
+    font-size: 1.125rem;
+  }
+
+  span:last-child {
+    display: flex;
+    align-items: center;
+    font-weight: 400;
+    font-size: 12px;
+  }
+`;
+
+const Status = styled.span`
+  display: inline-block;
+  width: 0.625rem;
+  height: 0.625rem;
+  background-color: #37d259;
+  border-radius: 50%;
+  margin-right: 0.25rem;
 `;
