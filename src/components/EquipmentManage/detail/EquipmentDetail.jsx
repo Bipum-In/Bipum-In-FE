7faef1873d197cfd727368fb4c59f6fb2c9a3a5c
Adult96 <djxor007@gmail.com>
2023-03-24 23:23:26 +0900

    feat: equipmentDetail 리덕스 thunk slice 구현

--- src/components/EquipmentManage/detail/EquipmentDetail.jsx
+++ src/components/EquipmentManage/detail/EquipmentDetail.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { getEquipmentDetail } from '../../../redux/modules/equipmentStatus';
 
 export default function EquipmentDetail({ detailId }) {
+  const dispatch = useDispatch();
+  const { getDetail, isDetailError } = useSelector(
+    state => state.equipmentStatus.equipmentDetail
+  );
+
+  useEffect(() => {
+    dispatch(getEquipmentDetail(detailId));
+  }, [detailId, dispatch]);
+
   return <DetailWrapper></DetailWrapper>;
 }
 
