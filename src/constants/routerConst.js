8e3f53a21c8918ed5f4172f5b818a62b77376fbb
9mean2 <o3omoomin@gmail.com>
2023-03-30 05:14:12 +0900

    style: userstockview 콘스탄스 추가

--- src/constants/routerConst.js
+++ src/constants/routerConst.js
@@ -8,6 +8,7 @@ const PATH = {
   ADMIN_EQUIPMENT_ADD: '/equipment-add',
   USER_DASHBOARD: '/user-dashboard',
   USER_REQUEST: '/user-request',
+  USER_STOCK_VIEW: '/user-stockview',
   RENDING: '/rending',
   BACK: -1,
   FRONT: 1,
--- src/router/router.js
+++ src/router/router.js
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
             element: <UserRequest />,
           },
           {
-            path: '/user-stockview',
+            path: ROUTER.PATH.USER_STOCK_VIEW,
             element: <UserStockView />,
           },
         ],
