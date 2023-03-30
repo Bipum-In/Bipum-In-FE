b245a5610dcf50dfc4183e4b5af14a8bef4633ea
Adult96 <djxor007@gmail.com>
2023-03-31 05:02:51 +0900

    fix: RequestModal 컴포넌트 RequestDetail을 AdminRequestDetail로 이름 변경

--- src/components/requestStatus/RequestModal.jsx
+++ src/components/requestStatus/RequestModal.jsx
@@ -2,23 +2,23 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { initDetail, requestDetail } from '../../redux/modules/requestStatus';
-import RequestDetail from './RequestDetail';
+import AdminRequestDetail from './AdminRequestDetail';
 
-export default function RequestModal({ isClose, detailId }) {
+export default function RequestModal({ isClose, detailId, path }) {
   const dispatch = useDispatch();
-  const { getDetail, isDetailLoading, isDetailError } = useSelector(
+  const { getDetail, isDetailError } = useSelector(
     state => state.requestStatus.requestDetail
   );
 
   useEffect(() => {
     dispatch(initDetail());
-    dispatch(requestDetail(detailId));
-  }, [detailId, dispatch]);
+    dispatch(requestDetail({ path, detailId }));
+  }, [path, detailId, dispatch]);
 
   return (
     <RequestModalWrapper>
       {isDetailError && <div>에러</div>}
-      {getDetail && <RequestDetail isClose={isClose} detail={getDetail} />}
+      {getDetail && <AdminRequestDetail isClose={isClose} detail={getDetail} />}
     </RequestModalWrapper>
   );
 }
