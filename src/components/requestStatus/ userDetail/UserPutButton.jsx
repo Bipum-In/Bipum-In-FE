60bfce56a0e9f1c284e48d2122c696d982000ed1
Seogun95 <ksj083895@gmail.com>
2023-04-01 02:57:38 +0900

    feat: userRequest 버튼 컴포넌트 추가 조건부에 따른 로직 처리

---
+++ src/components/requestStatus/ userDetail/UserPutButton.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from '../../../elements/Button';
+import STRING from '../../../constants/string';
+
+export default function UserPutButton({
+  requestType,
+  editMode,
+  acceptResult,
+  handleEditToggle,
+  editSupply,
+  editReturn,
+  editRepair,
+  editReport,
+}) {
+  const editFunctions = {
+    [STRING.REQUEST_NAME.SUPPLY]: editSupply,
+    [STRING.REQUEST_NAME.RETURN]: editReturn,
+    [STRING.REQUEST_NAME.REPAIR]: editRepair,
+    [STRING.REQUEST_NAME.REPORT]: editReport,
+  };
+
+  const editFunction = editFunctions[requestType];
+
+  return (
+    acceptResult !== STRING.REQUEST_STATUS.ACCEPT && (
+      <Button
+        mainBtn={'fill'}
+        onClick={editMode ? editFunction : handleEditToggle}
+      >
+        {editMode ? '저장' : '수정'}
+      </Button>
+    )
+  );
+}
