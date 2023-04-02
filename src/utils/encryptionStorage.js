3add2cafe3f1a87bd0636afd37349b86bcef87ce
Seogun95 <ksj083895@gmail.com>
2023-04-03 03:21:13 +0900

    feat: 암호화된 데이터 복호화해 받아오고 업데이트하는 로직 구현 완료

---
+++ src/utils/encryptionStorage.js
@@ -0,0 +1,71 @@
+import { encrypt, decrypt } from './encryption';
+import Storage from 'utils/localStorage';
+import QUERY from 'constants/query';
+
+const DEFAULT_STORAGE_DATA = {
+  checkUser: null,
+  deptName: null,
+  empName: null,
+  image: null,
+  isAdmin: null,
+  userId: null,
+};
+
+export function getEncryptionStorage() {
+  const encryptedData = Storage.getLocalStorageJSON(QUERY.STORAGE.LOCAL_NAME);
+
+  if (!encryptedData) {
+    return DEFAULT_STORAGE_DATA;
+  }
+
+  try {
+    const decryptedData = decrypt(encryptedData);
+    return { ...DEFAULT_STORAGE_DATA, ...decryptedData };
+  } catch (error) {
+    console.error(
+      '로컬 저장소 데이터 복호화 중 오류가 발생했습니다. : ',
+      error
+    );
+    return null;
+  }
+}
+
+export function updateEncryptionStorage(key, value) {
+  const encryptedData = Storage.getLocalStorageJSON(QUERY.STORAGE.LOCAL_NAME);
+
+  if (!encryptedData) {
+    return DEFAULT_STORAGE_DATA;
+  }
+
+  try {
+    const decryptedData = decrypt(encryptedData);
+    decryptedData[key] = value;
+    const updatedEncryptedData = encrypt(decryptedData);
+    Storage.setLocalStorageJSON(QUERY.STORAGE.LOCAL_NAME, updatedEncryptedData);
+  } catch (error) {
+    console.error(
+      '로컬 저장소 데이터 업데이트 중 오류가 발생했습니다. : ',
+      error
+    );
+    return null;
+  }
+}
+
+// export function getEncryptionStorage() {
+//   const encryptedData = Storage.getLocalStorageJSON(QUERY.STORAGE.LOCAL_NAME);
+
+//   if (!encryptedData) {
+//     return null;
+//   }
+
+//   try {
+//     const decryptedData = decrypt(encryptedData);
+//     return decryptedData;
+//   } catch (error) {
+//     console.error(
+//       '로컬 저장소 데이터 복호화 중 오류가 발생했습니다. : ',
+//       error
+//     );
+//     return null;
+//   }
+// }
