261b5a5c29bd6beba332886bc804371aeb93d2c4
Adult96 <djxor007@gmail.com>
2023-04-04 03:01:40 +0900

    style(SelectUser.jsx): 불필요한 컴포넌트 삭제

--- src/components/equipmentAdd/single/SelectUser.jsx
+++ src/components/equipmentAdd/single/SelectUser.jsx
@@ -12,36 +12,22 @@ export default function SelectUser({
 }) {
   return (
     <>
-      <DepName>
-        <SelectCategory
-          category={category[0]}
-          optionNullName={optionNullName[0]}
-          optionKey={optionKey[0]}
-          optionValueKey={optionValueKey[0]}
-          optionName={optionName[0]}
-          onChangeCategory={onChangeCategory[0]}
-        />
-      </DepName>
-      <UserName>
-        <SelectCategory
-          category={category[1]}
-          optionNullName={optionNullName[1]}
-          optionKey={optionKey[1]}
-          optionValueKey={optionValueKey[1]}
-          optionName={optionName[1]}
-          onChangeCategory={onChangeCategory[1]}
-        />
-      </UserName>
+      <SelectCategory
+        category={category[0]}
+        optionNullName={optionNullName[0]}
+        optionKey={optionKey[0]}
+        optionValueKey={optionValueKey[0]}
+        optionName={optionName[0]}
+        onChangeCategory={onChangeCategory[0]}
+      />
+      <SelectCategory
+        category={category[1]}
+        optionNullName={optionNullName[1]}
+        optionKey={optionKey[1]}
+        optionValueKey={optionValueKey[1]}
+        optionName={optionName[1]}
+        onChangeCategory={onChangeCategory[1]}
+      />
     </>
   );
 }
-
-const DepName = styled.div`
-  width: 5.8125rem;
-  height: 2.5rem;
-`;
-
-const UserName = styled.div`
-  width: 5.8125rem;
-  height: 2.5rem;
-`;
