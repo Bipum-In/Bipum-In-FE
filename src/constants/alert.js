c37a69cf45b89263a4e6d30c3b3679ab9227237f
Adult96 <djxor007@gmail.com>
2023-04-05 03:12:22 +0900

    feat(alert.js): ALERT 객체 image length check 함수 추가

--- src/constants/alert.js
+++ src/constants/alert.js
@@ -1,6 +1,9 @@
 const ALERT = {
   CHECK_EMPTY: '공백은 입력할 수 없습니다.',
   CHECK_DIFF_PW: '비밀 번호를 다시 확인해 주세요.',
+  CHECK_IMAGE_LENGTH(length) {
+    return `이미지는 ${length}장까지 등록 가능합니다.`;
+  },
 };
 
 export default ALERT;
