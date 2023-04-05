fab9f107dc010b6ee71b25668fe26ab886fa752f
Adult96 <djxor007@gmail.com>
2023-04-06 03:38:38 +0900

    feat(validation.js): excel sheet 유효성 검사 함수 구현

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,5 +1,6 @@
 import ALERT from 'constants/alert';
-import alertModal from 'utils/alertModal';
+import ARRAY from 'constants/array';
+import alertModal, { alertModalButton } from 'utils/alertModal';
 
 const Valid = {
   imgLengthCheck(img, length) {
@@ -10,6 +11,72 @@ const Valid = {
 
     return true;
   },
+
+  excelSheetCheck(sheetName, excel) {
+    const errorArray = validRoopExcelSheet(sheetName, excel);
+
+    if (errorArray.length) {
+      alertModalButton(false, ALERT.CHECK_EXCEL_SHEET(errorArray));
+      return false;
+    }
+
+    return true;
+  },
 };
+
+function validRoopExcelSheet(sheetNames, excel) {
+  const result = [];
+
+  sheetNames.forEach((sheetName, index) => {
+    const errorArray = validExcelSheet(excel[index]);
+    if (errorArray.length) {
+      result.push(`
+      sheet: ${sheetName} 
+      cell : ${errorArray.join(', ')}
+      `);
+    }
+  });
+
+  return result;
+}
+
+function validExcelSheet(excel) {
+  const errorArray = [];
+  const columnArray = ARRAY.EXCEL_COLUMN;
+  const columnTitleArray = ARRAY.MULTIPLE_HEADER;
+
+  let columnCnt = 0;
+  let rowCnt = 0;
+
+  const parseEmptyColumn = excel.filter(v => new Set(v).size !== 1);
+  const rowLength = parseEmptyColumn.length - 1;
+
+  while (columnCnt < columnArray.length) {
+    const checkRow = parseEmptyColumn[rowCnt][columnCnt];
+
+    if (checkRow) {
+      if (rowCnt === 0 && columnTitleArray[columnCnt] !== checkRow) {
+        errorArray.push(`${columnArray[columnCnt]}${rowCnt + 1}`);
+      }
+
+      rowCnt++;
+    }
+
+    if (!checkRow) {
+      if (rowCnt === 0 || columnCnt < 3) {
+        errorArray.push(`${columnArray[columnCnt]}${rowCnt + 1}`);
+      }
+
+      rowCnt++;
+    }
+
+    if (rowCnt === rowLength) {
+      columnCnt++;
+      rowCnt = 0;
+    }
+  }
+
+  return errorArray;
+}
 
 export default Valid;
