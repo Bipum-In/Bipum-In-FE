aedff1ae30eb7cb5bd6e50c35a7e9b1e79fb3c45
Seogun95 <ksj083895@gmail.com>
2023-04-07 16:51:39 +0900

    feat(DetailImage.jsx): 이미지 클릭시 이미지가 나타나도록 로직 수정

--- src/components/EquipmentManage/detail/DetailImage.jsx
+++ src/components/EquipmentManage/detail/DetailImage.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ImageAdd from '../../equipmentAdd/single/ImageAdd';
+import { ImgDetailModal } from 'elements/ImgModal';
 
 export default function DetailImage({ detail, preview, onChangeImage }) {
   const { image } = detail.supplyDetail;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(prev => !prev);
+  };
+
   return (
-    <ImgContainer>
-      {image ? (
-        <img src={image} alt="Img" />
-      ) : (
-        <ImageAdd preview={preview} onChangeimge={onChangeImage} />
-      )}
-    </ImgContainer>
+    <>
+      <ImgContainer>
+        {image ? (
+          <img src={image} alt="Img" onClick={handleClick} />
+        ) : (
+          <ImageAdd preview={preview} onChangeimge={onChangeImage} />
+        )}
+      </ImgContainer>
+      <ImgDetailModal src={image} isOpen={showModal} onClose={handleClick} />
+    </>
   );
 }
 
 const ImgContainer = styled.div`
   display: flex;
   margin-right: 5.9375rem;
-
+  cursor: pointer;
   img {
     width: 23.25rem;
     height: 23.25rem;
