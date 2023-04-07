f42af039eb075329efcd0db3e97beda9e28d7750
Seogun95 <ksj083895@gmail.com>
2023-04-08 05:14:27 +0900

    feat(CategorStatus.jsx): 공용비품 로직 추가

--- src/components/adminDashBoard/status/CategoryStatus.jsx
+++ src/components/adminDashBoard/status/CategoryStatus.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import AnchorBtn from '../AnchorBtn';
 import { v4 as uuidv4 } from 'uuid';
+
+import AnchorBtn from '../AnchorBtn';
+import Button from 'elements/Button';
 import { ReactComponent as ArrowIcon } from 'styles/commonIcon/arrowDown.svg';
+import { ReactComponent as Rotate } from 'styles/headerIcon/rotate.svg';
+
 import CategoryItems from '../../common/CategoryItems';
 import DashboardCard from '../DashboardCard';
+import UserDashboardCard from '../UserDashboardCard';
 import useSelectMenu from 'hooks/useSelectMenu';
+import { setCategoryData } from 'redux/modules/equipmentStatus';
+
 import STRING from 'constants/string';
 import ROUTER from 'constants/routerConst';
-import UserDashboardCard from '../UserDashboardCard';
-import { setCategoryData } from 'redux/modules/equipmentStatus';
 
 export default function CategoryStatus({
   isAdmin,
   getDashboard,
   setStatus,
   onDetailModal,
+  handleCommonSupplyDtos,
+  getCommonSupplyDtos,
+  showCommonSupplyDtos,
 }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -36,13 +44,8 @@ export default function CategoryStatus({
 
   const handleClickMenue = e => {
     const name = e.target.innerText;
-    getCategoryList(name, largeCategory);
     clickMenu(e);
     setStatus(STRING.CATEGORY[name] || '');
-  };
-
-  const getCategoryList = (name, categoryList) => {
-    return categoryList.filter(list => list.name === name);
   };
 
   const handleCategoryClick = (id, name) => {
@@ -60,38 +63,61 @@ export default function CategoryStatus({
               navigate(ROUTER.PATH.ADMIN.EQUIPMENT_MANAGEMENT);
             }}
           >
-            비품 목록
+            {isAdmin ? '비품 목록' : '사용중인 비품 목록'}
             <ArrowIcon />
           </AnchorBtn>
-          <CategoryItems
-            getCategory={menuStyle}
-            onClickMenu={handleClickMenue}
-          />
-          <EquipmentBottomContainer>
-            {supplyDtos.map(card =>
-              isAdmin ? (
-                <DashboardCard
-                  handleCategoryClick={handleCategoryClick}
-                  categoryId={card.categoryId}
-                  key={uuidv4()}
-                  totalCount={card.totalCount}
-                  categoryName={card.categoryName}
-                  useCount={card.useCount}
-                  repairCount={card.repairCount}
-                  stockCount={card.stockCount}
-                />
-              ) : (
-                <UserDashboardCard
-                  key={uuidv4()}
-                  image={card.image}
-                  status={card.status}
-                  supplyId={card.supplyId}
-                  supplyName={card.supplyName}
-                  categoryName={card.categoryName}
-                  onDetailModal={onDetailModal}
-                />
-              )
+          <CategoryListContainer>
+            {!isAdmin && (
+              <TransitionBtn mainBtn={'fill'} onClick={handleCommonSupplyDtos}>
+                <Rotate />
+                {showCommonSupplyDtos ? '개인 비품 전환' : ' 공용 비품 전환'}
+              </TransitionBtn>
             )}
+            <CategoryItems
+              getCategory={menuStyle}
+              onClickMenu={handleClickMenue}
+            />
+          </CategoryListContainer>
+          <EquipmentBottomContainer>
+            {showCommonSupplyDtos && getCommonSupplyDtos
+              ? getCommonSupplyDtos.data.map(card => (
+                  <UserDashboardCard
+                    key={uuidv4()}
+                    image={card.image}
+                    status={card.status}
+                    supplyId={card.supplyId}
+                    supplyName={card.supplyName}
+                    categoryName={card.categoryName}
+                    onDetailModal={onDetailModal}
+                    getCommonSupplyDtos={getCommonSupplyDtos}
+                    showCommonSupplyDtos={showCommonSupplyDtos}
+                  />
+                ))
+              : supplyDtos.map(card =>
+                  isAdmin ? (
+                    <DashboardCard
+                      handleCategoryClick={handleCategoryClick}
+                      categoryId={card.categoryId}
+                      key={uuidv4()}
+                      totalCount={card.totalCount}
+                      categoryName={card.categoryName}
+                      useCount={card.useCount}
+                      repairCount={card.repairCount}
+                      stockCount={card.stockCount}
+                    />
+                  ) : (
+                    <UserDashboardCard
+                      key={uuidv4()}
+                      image={card.image}
+                      status={card.status}
+                      supplyId={card.supplyId}
+                      supplyName={card.supplyName}
+                      categoryName={card.categoryName}
+                      onDetailModal={onDetailModal}
+                      getCommonSupplyDtos={getCommonSupplyDtos}
+                    />
+                  )
+                )}
           </EquipmentBottomContainer>
         </>
       )}
@@ -105,4 +131,20 @@ const EquipmentBottomContainer = styled.div`
   flex-wrap: wrap;
   width: 100%;
   gap: 1.25rem;
+`;
+
+const CategoryListContainer = styled.div`
+  ${props => props.theme.FlexRow};
+  width: 100%;
+`;
+
+const TransitionBtn = styled(Button)`
+  height: 100%;
+  margin: 0;
+  margin-right: 1.25rem;
+  padding: 0.625rem;
+  background-color: ${props => props.theme.color.blue.brandColor5};
+  svg * {
+    stroke: white;
+  }
 `;
