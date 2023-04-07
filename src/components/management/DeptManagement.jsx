903b7c791c500745353036ce9ec8c9cbea9f94aa
9mean2 <o3omoomin@gmail.com>
2023-04-08 04:58:16 +0900

    docs: 부서 및 권환 관리 , 협력업체 관리 페이지 생성

---
+++ src/components/management/DeptManagement.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import STRING from 'constants/string';
+
+export default function DeptManagement() {
+  return (
+    <Warpper>
+      <ManagementType>
+        <Title>{STRING.MANAGEMENT_TITLE.DEPTAUTH}</Title>
+      </ManagementType>
+    </Warpper>
+  );
+}
+
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+  margin-left: 2.5rem;
+`;
+
+const ManagementType = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 1.75rem;
+  width: 100%;
+  height: 3rem;
+`;
+
+const Warpper = styled.div`
+  ${props => props.theme.wh100};
+  height: 73.9vh;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+`;
