8192708baed1ea057a6092fea1434e4b3836d1ac
Seogun95 <ksj083895@gmail.com>
2023-04-10 02:55:40 +0900

    chore: Sentry 패키지 설치

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ import store from 'redux/config/configStore';
 import { ThemeProvider } from 'styled-components';
 import router from 'router/router';
 
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
