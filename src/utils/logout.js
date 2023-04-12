ebdd32b45877f7d07e1163763cbe1520e666709e
Adult96 <djxor007@gmail.com>
2023-04-13 05:58:33 +0900

    feat(logout.js): 로그아웃 코드 util폴더로 모듈화

---
+++ src/utils/logout.js
@@ -0,0 +1,10 @@
+import { removeCookie } from 'utils/cookie';
+import Storage from 'utils/localStorage';
+import QUERY from 'constants/query';
+
+export default function logout(callback) {
+  removeCookie(QUERY.COOKIE.COOKIE_NAME);
+  removeCookie(QUERY.COOKIE.REFRESH_NAME);
+  Storage.clearLocalStorage();
+  callback();
+}
