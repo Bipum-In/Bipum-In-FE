391296c7efe0151e35db849ad743dbfcafa882b1
Seogun95 <ksj083895@gmail.com>
2023-04-13 06:31:06 +0900

    feat(MyPage.jsx): 회원정보 수정 부모 페이지 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import MyDetails from 'components/myPage/myDetails';
+import CheckPwPage from 'components/myPage/CheckPwPage';
 
 export default function MyPage() {
+  const [editPage, setEditPage] = useState('checkPw');
   const { getUserInfo } = useSelector(state => state.userInfo.userInfoList);
 
-  return <>{getUserInfo && <MyDetails getUserInfo={getUserInfo} />}</>;
+  return (
+    <>
+      {getUserInfo && (
+        <>
+          {editPage === 'checkPw' ? (
+            <CheckPwPage setEditPage={setEditPage} />
+          ) : (
+            <MyDetails getUserInfo={getUserInfo} setEditPage={setEditPage} />
+          )}
+        </>
+      )}
+    </>
+  );
 }
