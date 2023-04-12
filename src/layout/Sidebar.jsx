84f9cfe116660ccaa419eb3b2ab69e09bf8a132a
Seogun95 <ksj083895@gmail.com>
2023-04-12 17:30:16 +0900

    design(Sidebar.jsx): 사이드바 반응형 미디어 쿼리로 변경

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styled, { useTheme } from 'styled-components';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-
 import { CategoryItemLeft } from 'components/layout/CategoryItem';
 
 import Logo from 'components/layout/Logo';
@@ -13,59 +12,36 @@ import { ReactComponent as Dashboard } from 'styles/sidebarIcon/dashboard.svg';
 import { ReactComponent as List } from 'styles/sidebarIcon/list.svg';
 import { ReactComponent as Management } from 'styles/sidebarIcon/management.svg';
 import { ReactComponent as Setting } from 'styles/headerIcon/setting.svg';
-import { ReactComponent as ArrowDown } from 'styles/commonIcon/arrowDown.svg';
+// import { ReactComponent as ArrowDown } from 'styles/commonIcon/arrowDown.svg';
 
 import ROUTER from 'constants/routerConst';
 import STRING from 'constants/string';
 import ARRAY from 'constants/array';
 import useOutsideClick from 'hooks/useOutsideClick';
-import { getEncryptionStorage } from '../utils/encryptionStorage';
 
 export default function Sidebar({
   isSidebarHidden,
   setIsSidebarHidden,
-  isMobileView,
+  handleClickCategory,
+  isAdmin,
+  isAdminStr,
+  pathname,
 }) {
-  const { isAdmin } = getEncryptionStorage();
-  const isAdminStr = STRING.IS_ADMIN(isAdmin);
-
   const navigate = useNavigate();
   const theme = useTheme();
   const categoryIcons = isAdmin
     ? [<Dashboard />, <List />, <Management />, <Add />]
     : [<Dashboard />, <Add />, <List />, <Eye />];
 
-  const { pathname } = useLocation();
-
-  const handleClickCategory = e => {
-    const name = e.target.innerText;
-    if (!name) {
-      const targetPath = isAdmin
-        ? ROUTER.PATH.ADMIN.DASHBOARD
-        : ROUTER.PATH.USER.DASHBOARD;
-      navigate(targetPath);
-    }
-    const routerPathArray = Object.values(ROUTER.PATH[isAdminStr]);
-    const index = Object.values(STRING.SIDEBAR[isAdminStr]).findIndex(
-      sidebarName => sidebarName === name
-    );
-    if (index >= 0) {
-      navigate(routerPathArray[index]);
-    }
-    if (desktopSize) {
-      setIsSidebarHidden(true);
-    }
-  };
-
   const desktopSize = window.innerWidth <= theme.screen.desktopSize;
 
   const dropDownRef = useOutsideClick(() => {
     setIsSidebarHidden(true);
   }, desktopSize);
+
   return (
     <>
-      <SidebarWrapper isHidden={isSidebarHidden} ref={dropDownRef}>
-        {isMobileView && <ArrowDownIcon onClick={handleClickCategory} />}
+      <SidebarWrapper isSidebarVisible={isSidebarHidden} ref={dropDownRef}>
         <LogoContainer onClick={handleClickCategory}>
           <Logo />
         </LogoContainer>
@@ -116,8 +92,11 @@ const SidebarWrapper = styled.aside`
   box-shadow: -0.3125rem 0 1.5625rem 0 rgba(0, 0, 0, 0.25);
   border-radius: 0 2.5rem 2.5rem 0;
   z-index: 1000;
-  transform: ${({ isHidden }) => (isHidden ? 'translateX(-100%)' : 'none')};
   transition: transform 0.3s ease-in-out;
+  @media (max-width: ${props => props.theme.screen.desktop}) {
+    transform: ${({ isSidebarVisible }) =>
+      isSidebarVisible ? 'translateX(-100%)' : 'none'};
+  }
 `;
 
 const LogoContainer = styled.div`
@@ -146,8 +125,4 @@ const ManageManetContainer = styled.div`
   width: 100%;
   margin-top: auto;
   margin-bottom: 3.625rem;
-`;
-
-const ArrowDownIcon = styled(ArrowDown)`
-  position: absolute;
 `;
