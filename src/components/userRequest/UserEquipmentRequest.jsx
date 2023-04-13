02ca89a242a407a6cee5ed7e88eb9f6de96237cc
Adult96 <djxor007@gmail.com>
2023-04-13 16:57:06 +0900

    style(UserEquipmentRequest.jsx): 사용하지 않는 코드 삭제

--- src/components/userRequest/UserEquipmentRequest.jsx
+++ src/components/userRequest/UserEquipmentRequest.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import { styles } from '../common/commonStyled';
 
 import Button from 'elements/Button';
