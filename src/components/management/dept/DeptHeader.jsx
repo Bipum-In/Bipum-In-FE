d41d323f5898cbd9936987550989ee80f3a5812c
9mean2 <o3omoomin@gmail.com>
2023-04-17 07:19:07 +0900

    feat(DeptHeader): 부서추가 기능 구현 완료

---
+++ src/components/management/dept/DeptHeader.jsx
@@ -0,0 +1,178 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Search } from 'styles/commonIcon/search.svg';
+import Input from 'elements/Input';
+import Button from 'elements/Button';
+import { ReactComponent as WhiteAdd } from 'styles/commonIcon/whiteAdd.svg';
+import { ReactComponent as CancelInput } from 'styles/commonIcon/cancelInput.svg';
+
+import { useModalState } from 'hooks/useModalState';
+import Axios from 'api/axios';
+import { CustomModal } from 'elements/Modal';
+
+export default function DeptHeader({
+  setSelectName,
+  containerHeaderRef,
+  keyword,
+  setKeyword,
+  handleSubmit,
+  setDeptName,
+  deptName,
+}) {
+  const [addPatnerModal, setAddPatnerModal] = useModalState();
+  const handleModalClose = () => setAddPatnerModal(false);
+  const handleDeptClear = () => setDeptName('');
+
+  return (
+    <RequestShowTitle ref={containerHeaderRef}>
+      <Title>{setSelectName}</Title>
+      <SearchSelect>
+        <SearchContainer>
+          <SearchIconContainer>
+            <Button>
+              <Search />
+            </Button>
+          </SearchIconContainer>
+          <Input
+            value={keyword}
+            setState={setKeyword}
+            placeholder="검색어를 입력해주세요 (이름, 전화번호 등)"
+          />
+        </SearchContainer>
+
+        <BtnContainer>
+          <Button mainBtn="fill" type="button" onClick={setAddPatnerModal}>
+            <WhiteAdd />
+            부서 추가
+          </Button>
+          <CustomModal
+            width={'345px'}
+            isOpen={addPatnerModal}
+            onClose={handleModalClose}
+            handleModalClose={handleModalClose}
+            text={'추가'}
+            submit={() => {
+              handleSubmit();
+              handleModalClose();
+            }}
+          >
+            부서 추가
+            <AddInputBox>
+              <Input
+                type="text"
+                value={deptName}
+                setState={e => setDeptName(e.target.value)}
+                placeholder="부서명을 입력해 주세요"
+              />
+              {deptName && (
+                <CancelInputWrapper onClick={handleDeptClear}>
+                  <CancelInput />
+                </CancelInputWrapper>
+              )}
+            </AddInputBox>
+          </CustomModal>
+        </BtnContainer>
+      </SearchSelect>
+    </RequestShowTitle>
+  );
+}
+
+const CancelInputWrapper = styled.div`
+  top: 50%;
+  margin: auto;
+  margin-top: 1.3125rem;
+  transform: translateY(-50%);
+  cursor: pointer;
+  padding-right: 8px;
+`;
+
+const AddInputBox = styled.div`
+  margin-top: 20px;
+  background: #f7faff;
+  border: 1px solid ${props => props.theme.color.blue.brandColor3};
+  border-radius: 0.375rem;
+  color: ${props => props.theme.color.blue.brandColor7};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 8px;
+  input {
+    padding: 8px;
+    width: 100%;
+    font-size: 1rem;
+    padding: 0.5rem 0.5rem 0.5rem 0;
+    font-size: 1rem;
+    line-height: 1.1875rem;
+    ::placeholder {
+      color: ${props => props.theme.color.blue.brandColor7};
+    }
+  }
+`;
+
+const BtnContainer = styled.div`
+  button {
+    background-color: ${props => props.theme.color.blue.brandColor5};
+    margin-right: 2.5625rem;
+    width: 6.75rem;
+    height: 2.125rem;
+  }
+`;
+
+const RequestShowTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 1.375rem;
+  border-bottom: 1px solid ${props => props.theme.color.grey.brandColor3};
+`;
+
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 1.25rem;
+  margin-top: 1.75rem;
+  margin: 1.75rem 2.5rem 0 2.5rem;
+  white-space: nowrap;
+`;
+
+const SearchSelect = styled.div`
+  ${props => props.theme.FlexRow}
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  margin-top: 1.5rem;
+`;
+
+const SearchContainer = styled.div`
+  ${props => props.theme.FlexRow}
+  ${props => props.theme.FlexCenter}
+  max-width: 28.375rem;
+  width: 100%;
+  height: 2.125rem;
+  background-color: ${props => props.theme.color.grey.brandColor1};
+  margin: 0;
+  margin-right: 1rem;
+  border-radius: 0.5rem;
+
+  input {
+    width: 100%;
+    font-size: 1rem;
+    padding: 0.5rem 0.5rem 0.5rem 0;
+  }
+
+  @media (max-width: 64.5625rem) {
+    width: 15rem;
+
+    input::-webkit-input-placeholder {
+      color: transparent;
+    }
+    input:-ms-input-placeholder {
+      color: transparent;
+    }
+  }
+`;
+
+const SearchIconContainer = styled.div`
+  padding: 0 1rem;
+  button {
+    padding: 0;
+  }
+`;
