58fafd22ea36379eb234e9a7c3819c559bebb3d1
9mean2 <o3omoomin@gmail.com>
2023-04-17 21:46:23 +0900

    refactor(DeptManagement): userShowbody 컴폰너트 분리

--- src/components/management/DeptManagement.jsx
+++ src/components/management/DeptManagement.jsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import DeptHeader from './dept/DeptHeader';
-import { v4 as uuidv4 } from 'uuid';
 import Axios from 'api/axios';
 import STRING from 'constants/string';
-import Button from 'elements/Button';
 import DeptSidebar from './dept/DeptSidebar';
-import { CustomModal } from 'elements/Modal';
 import { useModalState } from 'hooks/useModalState';
-
+import UserShowBody from './dept/UserShowBody';
 const axios = new Axios(process.env.REACT_APP_SERVER_URL);
 
 export default function DeptManagement() {
@@ -16,17 +13,11 @@ export default function DeptManagement() {
   const [deptList, setDeptList] = useState([]);
   const [deptName, setDeptName] = useState('');
   const [editedDeptName, setEditedDeptName] = useState('');
-  const [selectedDeptId, setSelectedDeptId] = useState(null);
+  const [selectedDeptId, setSelectedDeptId] = useState(1);
   const [employeeList, setEmployeeList] = useState([]);
-  const [keyword, setKeyword] = useState('');
   const [idModalDept, setIdModalDept] = useState(null);
-  const [idauthModal, setIdauthModal] = useState(null);
-
+  const [filteredEmployeeList, setFilteredEmployeeList] = useState([]);
   const [userdeleteModal, setUserDeleteModal] = useModalState();
-  const [authModal, setAuthModal] = useModalState();
-
-  const deleteModalClose = () => setUserDeleteModal(false);
-  const authModalClose = () => setAuthModal(false);
 
   const updateDeptList = () =>
     axios.get('/api/dept').then(response => setDeptList(response.data.data));
@@ -42,9 +33,6 @@ export default function DeptManagement() {
   useEffect(() => {
     updateDeptList();
   }, []);
-
-  const handleChangeKeyword = e => setKeyword(e.target.value);
-
   useEffect(() => {
     if (selectedDeptId !== null) {
       axios.get(`/api/dept/${selectedDeptId}`).then(response => {
@@ -101,11 +89,11 @@ export default function DeptManagement() {
         <DeptHeaderWrapper>
           <DeptHeader
             setSelectName={ManagementTitle}
-            keyword={keyword}
-            setKeyword={handleChangeKeyword}
             handleSubmit={handleSubmit}
             deptName={deptName}
             setDeptName={setDeptName}
+            employeeList={employeeList}
+            setFilteredEmployeeList={setFilteredEmployeeList}
           />
         </DeptHeaderWrapper>
         <ContentWrapper>
@@ -116,193 +104,27 @@ export default function DeptManagement() {
               editedDeptName={editedDeptName}
               setEditedDeptName={setEditedDeptName}
               updateDeptList={updateDeptList}
+              activeDeptId={selectedDeptId}
             />
           </Sidebar>
-          <RequestShowBody>
-            {employeeList.map(employee => (
-              <Test key={uuidv4()}>
-                <Teest>
-                  <Ttteeest>
-                    <td>
-                      <img src={employee.image} alt={employee.empName} />
-                    </td>
-                    <td>{employee.empName}</td>
-                    <td>{employee.phone}</td>
-                    <td>{employee.username}</td>
-
-                    <td>
-                      {employee.authority === null && (
-                        <Button
-                          mainBtn="border"
-                          type="button"
-                          onClick={() => {
-                            setIdauthModal(employee.userId);
-                            setAuthModal(true);
-                          }}
-                        >
-                          공용 비품 책임자 선임
-                        </Button>
-                      )}
-                      {employee.authority === '공용 비품 책임자' && (
-                        <Button
-                          mainBtn="border"
-                          type="button"
-                          onClick={() => {
-                            setIdauthModal(employee.userId);
-                            setAuthModal(true);
-                          }}
-                        >
-                          공용 비품 책임자 해임
-                        </Button>
-                      )}
-                      {employee.authority && employee.authority && (
-                        <Span>공용 비품 책임자</Span>
-                      )}
-                    </td>
-                    <td>
-                      <Button
-                        mainBtn="border"
-                        type="button"
-                        onClick={() => {
-                          setIdModalDept(employee.userId);
-                          setUserDeleteModal(true);
-                        }}
-                      >
-                        삭제
-                      </Button>
-                      <CustomModal
-                        isOpen={
-                          userdeleteModal && idModalDept === employee.userId
-                        }
-                        onClose={deleteModalClose}
-                        submit={() => {
-                          handleDeleteUser(employee.userId);
-                          setUserDeleteModal(false);
-                          setIdModalDept(null);
-                        }}
-                        text={'네'}
-                      >
-                        {employee.empName} 님을 삭제 하시겠습니까?
-                      </CustomModal>
-
-                      <CustomModal
-                        isOpen={authModal && idauthModal === employee.userId}
-                        onClose={authModalClose}
-                        submit={() => {
-                          handleAssignRole(employee.userId);
-                          setAuthModal(false);
-                          setIdauthModal(null);
-                        }}
-                        text={'네'}
-                      >
-                        {employee.authority === null ? (
-                          <>
-                            {employee.empName} 님을 공용 비품 책임자로
-                            <p>선임하시겠습니까?</p>
-                          </>
-                        ) : (
-                          <>
-                            {employee.empName} 님의 공용 비품 책임자 권한을
-                            <p>해임하시겠습니까?</p>
-                          </>
-                        )}
-                      </CustomModal>
-                    </td>
-                  </Ttteeest>
-                </Teest>
-              </Test>
-            ))}
-          </RequestShowBody>
+          <UserShowBody
+            filteredEmployeeList={filteredEmployeeList}
+            setUserDeleteModal={setUserDeleteModal}
+            userdeleteModal={userdeleteModal}
+            setIdModalDept={setIdModalDept}
+            idModalDept={idModalDept}
+            handleDeleteUser={handleDeleteUser}
+            handleAssignRole={handleAssignRole}
+          />
         </ContentWrapper>
       </DeptWrapper>
     </>
   );
 }
 
-const RequestShowBody = styled.div`
-  width: 100%;
-  overflow-x: auto;
-
-  table {
-    width: 100%;
-    table-layout: auto;
-  }
-
-  tbody {
-    justify-content: center;
-  }
-
-  tr {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-    gap: 1.875rem;
-    line-height: 3.3125rem;
-    padding: 0.8125rem 8.6875rem;
-    width: 100%;
-    border-bottom: 0.0625rem solid
-      ${props => props.theme.color.grey.brandColor4};
-    :hover {
-      background-color: ${props => props.theme.color.blue.brandColor2};
-    }
-  }
-  tr:hover {
-    background-color: ${props => props.theme.color.blue.brandColor2};
-    Button {
-      display: block;
-      background-color: white;
-    }
-  }
-  Button {
-    display: none;
-  }
-
-  td {
-    flex: 1;
-    text-align: center;
-    text-overflow: ellipsis;
-    font-size: 0.875rem;
-    overflow: hidden;
-    white-space: nowrap;
-    position: relative;
-  }
-
-  td img {
-    display: flex;
-    width: 4.75rem;
-    height: 4.75rem;
-    border-radius: 0.375rem;
-  }
-`;
-
-const Span = styled.div`
-  background-color: ${props => props.theme.color.blue.brandColor6};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 0.375rem;
-  width: 7rem;
-  height: 2.5625rem;
-  color: white;
-`;
-
-const Ttteeest = styled.tr``;
-
-const Teest = styled.tbody`
-  display: flex;
-  flex-direction: row;
-`;
-
-const Test = styled.table`
-  display: flex;
-  flex-direction: column;
-`;
-
 const DeptWrapper = styled.div`
   ${props => props.theme.wh100};
-  display: flex;
-  flex-direction: column;
+  ${props => props.theme.FlexCol};
 `;
 
 const DeptHeaderWrapper = styled.div`
