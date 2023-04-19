31efccb6ff2ec3e991efae2af127e1b62d7b0ded
Seogun95 <ksj083895@gmail.com>
2023-04-20 05:58:31 +0900

    feat(AddMultipleItem.jsx): formatDate 함수 코드 변경

--- src/components/equipmentAdd/AddMultipleItem.jsx
+++ src/components/equipmentAdd/AddMultipleItem.jsx
@@ -182,6 +182,7 @@ export default function AddMultipleItem() {
   };
 
   const formatDate = date => {
+    date.setHours(date.getHours() + 1);
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     const dd = String(date.getDate()).padStart(2, '0');
